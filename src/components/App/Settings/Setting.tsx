3001b7cd5c973bc21a1dcb9d0d03182aaf26f179
MartinBINARD <martin.binard.dev@gmail.com>
2023-09-01 23:17:43 +0200

    Ajout bouton ouverture Settings

--- src/components/App/Settings/Setting.tsx
+++ src/components/App/Settings/Setting.tsx
@@ -2,29 +2,36 @@ import './Setting.scss';
 
 function Settings() {
   return (
-    <aside className="settings">
-      <button type="button" className="settings-close-button">
-        X
-      </button>
-
-      <form className="settings-form">
-        <input
-          type="email"
-          className="settings-form-input"
-          placeholder="Email"
-          aria-label="email"
-        />
-        <input
-          type="password"
-          className="settings-form-input"
-          placeholder="Mot de passe"
-          aria-label="Mot de passe"
-        />
-        <button type="submit" className="settings-form-button">
-          Envoyer
+    <>
+      <aside className="settings">
+        <button type="button" className="settings-close-button">
+          X
         </button>
-      </form>
-    </aside>
+
+        <form className="settings-form">
+          <input
+            type="email"
+            className="settings-form-input"
+            placeholder="Email"
+            aria-label="email"
+          />
+          <input
+            type="password"
+            className="settings-form-input"
+            placeholder="Mot de passe"
+            aria-label="Mot de passe"
+          />
+          <button type="submit" className="settings-form-button">
+            Envoyer
+          </button>
+        </form>
+      </aside>
+      <aside className="open-settings active">
+        <button type="button" className="open-settings-button">
+          +
+        </button>
+      </aside>
+    </>
   );
 }
 
